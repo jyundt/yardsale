2e8c936051a9ea6cc54e8f62f8bb943a3aeec297
Jacob Yundt <jyundt@gmail.com>
2017-05-27 09:48:24 -0400

    Include redis cache and try/except for gspread auth error.

--- yardsale.py
+++ yardsale.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import gspread
+import redis
+import json
+import time
 from flask import Flask, render_template
 from flask_script import Manager, Shell
 from oauth2client.service_account import ServiceAccountCredentials
@@ -8,39 +11,68 @@ from mapbox import Geocoder
 app = Flask(__name__)
 manager = Manager(app)
 
+def get_worksheet():
+    """Auth to GApps and return a sheet"""
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    workbook = client.open_by_key('12OdDOCSp67kNgb1eVVamZJWpZQUcUyVAiZOghMamnJU')
+    sheet = workbook.get_worksheet(0)
+    return sheet
+
+
+def add_lon_lat(sheet):
+    """Update the sheet with lat/lon
+    I didn't really spend the proper time debugging this behavior, but the
+    oauth client will randomly time out and start to throw 401 errors.
+    I added a simple try/except block to try to catch this error and
+    re-auth if necessary.
+    """
+    try:
+        spreadsheet_list = sheet.get_all_values()
+    except gspread.exceptions.RequestError:
+        sheet = get_worksheet()
+        spreadsheet_list = sheet.get_all_values()
+    for row in range(1,len(spreadsheet_list)):
+        if spreadsheet_list[row][3] is '':
+            geocoder_response = geocoder.forward(spreadsheet_list[row][0],bbox=highland_park_bbox)
+            if geocoder_response.geojson()['features']:
+                lon, lat= geocoder_response.geojson()['features'][0]['center']
+                sheet.update_cell(row+1,4, lat)
+                sheet.update_cell(row+1,5, lon)
+    return sheet
+
+def get_all_data(redis_conn):
+    """Query redis to see if we have a cached JSON blob of gspread data"""
+    all_data = []
+    if redis_conn.get('all_data') is None:
+        sheet = get_worksheet()
+        sheet = add_lon_lat(sheet)
+        all_data = sheet.get_all_records()
+        redis_conn.set('all_data', json.dumps(all_data), 300)
+    else:
+        all_data = json.loads(redis_conn.get('all_data'))
+
+    return all_data
 def make_shell_context():
     return dict(app=app)
 
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
 highland_park_bbox = 	[-79.936351, 40.464845, -79.908334, 40.484]
 highland_park_center = [-79.9195, 40.4723]
 geocoder = Geocoder()
-
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
-workbook = client.open_by_key('12OdDOCSp67kNgb1eVVamZJWpZQUcUyVAiZOghMamnJU')
-sheet = workbook.get_worksheet(0)
+redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 
 
 @app.route('/')
 def index():
-    spreadsheet_list = sheet.get_all_values()
-    for row in range(1,len(spreadsheet_list)):
-        if spreadsheet_list[row][3] is '':
-            geocoder_response = geocoder.forward(spreadsheet_list[row][0], bbox=highland_park_bbox)
-            if geocoder_response.geojson()['features']:
-                lon, lat= geocoder_response.geojson()['features'][0]['center']
-                sheet.update_cell(row+1,4, lat)
-                sheet.update_cell(row+1,5, lon)
- 
     
-    #print sheet.get_all_records()
-    return render_template('index.html', highland_park_center=list(reversed(highland_park_center)), locations=sheet.get_all_records())
+    return render_template('index.html',
+                           highland_park_center=list(reversed(highland_park_center)),
+                           locations=get_all_data(redis_conn))
 
 if __name__ == '__main__':
     manager.run()
